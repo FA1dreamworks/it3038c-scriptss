e3e8a63f00c36e19a4f7e66a32885138cb320b2d
FA1dreamworks <nguyenwa@mail.uc.edu>
2022-10-08 21:34:43 -0400

    Project1

--- Project1/tellingTime.py
+++ Project1/tellingTime.py
@@ -1,3 +1,32 @@
 from datetime import date;
+import time;
 today= date.today();
-print("Todays date is: " + today);
+
+print("Do you want to know the time? ")
+answer = input();
+if answer == 'yes' or answer == 'Yes':
+    print("The time is: " + time.strftime("%I:%M:%S %p"))
+elif(answer == 'no' or answer == 'No'):
+    print("Thank you for using the telling time service!")
+    exit()
+elif(answer != 'yes' or 'no'):
+    print("That is not a correct response, please try again.")
+
+print("Do you want to know today's date? ")
+answer = input();
+if answer == 'yes' or answer == 'Yes':
+    print("Todays date is: ", today)
+elif(answer == 'no' or answer == 'No'):
+    print("Thank you for using the telling time service!")
+    exit()
+elif(answer != 'yes' or 'no'):
+    print("That is not a correct response, please try again.")
+
+print("For the full time and date please press F.")
+full= input();
+if full == 'F':
+    print("Today is: " + time.strftime("%B %d, %Y \nTime: %I:%M:%S %p"))
+elif(full != 'F'):
+    print("That is not a correct response. Thank you!")
+    exit()
+
