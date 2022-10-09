06273e6119442a91c543f591a3004149c3058d85
FA1dreamworks <nguyenwa@mail.uc.edu>
2022-10-08 22:09:41 -0400

    Project1

---
+++ Project1/README.md
@@ -0,0 +1,22 @@
+================
+Welcome to tellingTime! This program is a quick and easy way to tell you the time and date of the current day. 
+If you are ever in a rush or just stuck on what day it is, hop onto tellingTime!
+
+
+================
+tellingTime will be ran on python's editor, so when you first start this program,
+    Type in python tellingTime.py to run. 
+    It will then ask you a series of questions, answer with the following of yes or no or what it asks you to enter.
+    The program will display the correct time or date if directed and will also ask as a final question if you would 
+        like to get the full date and time.  
+    DISCLAIMER: ANSWER WITH ANYTHING OTHER THAN THAN DIRECTED, IT WILL EXIT THE PROGRAM AND YOU HAVE TO RERUN IT. 
+    Please pay close attention to what it is asking, otherwise it will exit the program. 
+
+
+
+================
+
+Hope you enjoy using tellingTime, 
+
+Thank you, 
+Will Nguyen
--- Project1/tellingTime.py
+++ Project1/tellingTime.py
@@ -12,22 +12,20 @@ elif(answer == 'no' or answer == 'No'):
 elif(answer != 'yes' or 'no'):
     print("That is not a correct response, please try again.")
     exit()
-    
-print("Do you want to know today's date? ")
-answer = input();
-if answer == 'yes' or answer == 'Yes':
-    print("Todays date is: ", today)
-elif(answer == 'no' or answer == 'No'):
-    print("Thank you for using the telling time service!")
-    exit()
-elif(answer != 'yes' or 'no'):
-    print("That is not a correct response, please try again.")
-    exit()
 
-print("For the full time and date please press F.")
-full= input();
+print("If all you need is the time type in exit, if you need the \ndate type in date.")
+exitDate=input()
+if exitDate == 'date':
+    print("Todays date is: ", today)
+elif(exitDate == 'exit'):
+    exit();
+
+print("For the full time and date please press F, otherwise type exit.")
+full = input();
 if full == 'F':
     print("Today is: " + time.strftime("%B %d, %Y \nTime: %I:%M:%S %p"))
+elif(full == 'exit'):
+    exit();
 elif(full != 'F'):
     print("That is not a correct response. Thank you!")
     exit()
