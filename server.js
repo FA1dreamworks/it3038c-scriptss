c27c774aa2a0c97b320f7ff03916863687d71b92
FA1dreamworks <nguyenwa@mail.uc.edu>
2022-10-18 11:59:44 -0400

    changes

---
+++ server.js
@@ -0,0 +1,44 @@
+const http = require("http");
+const fs = require("fs");
+const os = require("os");
+const ip = require('ip');
+
+ http.createServer((req, res) => {
+	if (req.url === "/") {
+		fs.readFile("./public/index.html", "UTF-8", (err, body) => {
+			res.writeHead(200, {"Content-Type": "text/html"});
+			res.end(body);
+		});
+	} else if(req.url.match("/sysinfo")) {
+		myHostName=os.hostname();
+		html=`
+			<!DOCTYPE html>
+			<html>
+			<head>
+			<title>Node Js Response</title>
+			</head>
+			<body>
+			<p>Hostname: ${myHostName}</p>
+			<p>IP: ${ip.address()}</p>
+			<p>Server Uptime: </p>
+			<p>Total Memory: </p>
+			<p>Free Memory: </p>
+			<p>Number of CPUs: </p>
+			</body>
+			</html>`
+
+	} else {
+		res.writeHead(404, {"Content-Type": "text/plain"});
+		res.end(`404 File Not Found at ${req.url}`);
+	}
+ }).listen(3000);
+
+const server = http.createServer((req, res) => {
+	res.writeHead(200, {"Content-type": "text/text"});
+
+	res.end('Hello from Node!')
+})
+
+server.listen(3000)
+
+console.log("Server listening on port 8000")
---
+++ test.js
@@ -0,0 +1,4 @@
+console.log("Hello, World")
+
+const hello = "Hello from Node JS Variable!"
+console.log(hello)
